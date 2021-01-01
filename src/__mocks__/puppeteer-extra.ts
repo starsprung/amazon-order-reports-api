836d678a9729a293679f8eb900e49eec80ae7a31
Shaun Starsprung <s@starsprung.com>
2020-12-31 18:00:56 -0800

    feat: get rid of getOrders, add getItems based on CSV reports
    BREAKING CHANGE: getOrders removed

--- src/__mocks__/puppeteer-extra.ts
+++ src/__mocks__/puppeteer-extra.ts
@@ -1,17 +1,27 @@
-import { Browser, Page, Response } from 'puppeteer';
+import { Browser, CDPSession, Page, Response, Target } from 'puppeteer';
 
 const response = ({
   ok: jest.fn()
 } as unknown) as Response;
 
+const cdpSession = ({
+  send: jest.fn()
+} as unknown) as CDPSession;
+
+const target = ({ createCDPSession: jest.fn() } as unknown) as Target;
+
 const page = ({
   $: jest.fn(),
+  $x: jest.fn(),
   $$eval: jest.fn(),
   click: jest.fn(),
   goto: jest.fn(),
+  select: jest.fn(),
+  target: jest.fn(),
   type: jest.fn(),
   url: jest.fn(),
-  waitForNavigation: jest.fn()
+  waitForNavigation: jest.fn(),
+  waitForResponse: jest.fn()
 } as unknown) as Page;
 
 const browser = ({
@@ -21,8 +31,10 @@ const browser = ({
 
 export const mocks = {
   browser,
+  cdpSession,
   page,
-  response
+  response,
+  target
 };
 
 export default {
